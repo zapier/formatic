e55910e69b6d51af4abff5c5e9057b9b7ba83019
Justin Deal <justin.deal@zapier.com>
2015-11-24 16:55:25 -0600

    live server now webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+require('es6-promise').polyfill();
+
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './demo/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['react-hot-loader', 'babel-loader'],
+      include: [path.join(__dirname, 'lib'), path.join(__dirname, 'demo')]
+    }, {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader?importLoaders=1'
+    }]
+  }
+};
