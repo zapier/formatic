4721626b2da9b9f6461eab4aa52755faaf9db62b
stevemolitor <stevemolitor@gmail.com>
2015-12-23 15:37:27 -0600

    disable remove last item for array control

--- build/lib/components/helpers/array-item-control.js
+++ build/lib/components/helpers/array-item-control.js
@@ -37,10 +37,11 @@ module.exports = React.createClass({
     var field = this.props.field;
 
     var isLastItem = field.fieldIndex === 0 && field.parent.value.length === 1;
-    var removeItemControl = undefined;
-    if (!isLastItem || config.isRemovalOfLastArrayItemAllowed(field)) {
-      removeItemControl = config.createElement('remove-item', { field: field, onClick: this.onRemove, onMaybeRemove: this.props.onMaybeRemove });
-    }
+
+    var removeItemControl = config.createElement('remove-item', {
+      field: field, onClick: this.onRemove, onMaybeRemove: this.props.onMaybeRemove,
+      readOnly: isLastItem && !config.isRemovalOfLastArrayItemAllowed(field)
+    });
 
     return R.div({ className: cx(this.props.classes) }, removeItemControl, this.props.index > 0 ? config.createElement('move-item-back', { field: field, onClick: this.onMoveBack }) : null, this.props.index < this.props.numItems - 1 ? config.createElement('move-item-forward', { field: field, onClick: this.onMoveForward }) : null);
   }
