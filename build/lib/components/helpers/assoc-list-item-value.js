db2bc777cd54a9601fe2cae7e83c38e8c71c57f7
stevemolitor <stevemolitor@gmail.com>
2016-01-15 16:58:18 -0600

    add new build files

---
+++ build/lib/components/helpers/assoc-list-item-value.js
@@ -0,0 +1,33 @@
+// # assoc-item-value component
+
+/*
+Render the value of an object item.
+*/
+
+'use strict';
+
+var React = require('react');
+var R = React.DOM;
+var cx = require('classnames');
+
+module.exports = React.createClass({
+
+  displayName: 'AssocListItemValue',
+
+  mixins: [require('../../mixins/helper')],
+
+  onChangeField: function onChangeField(newValue, info) {
+    this.props.onChange(this.props.index, newValue, info);
+  },
+
+  render: function render() {
+    return this.renderWithConfig();
+  },
+
+  renderDefault: function renderDefault() {
+    var config = this.props.config;
+    var field = this.props.field;
+
+    return R.div({ className: cx(this.props.classes) }, config.createFieldElement({ field: field, onChange: this.onChangeField, plain: true, onAction: this.onBubbleAction }));
+  }
+});
