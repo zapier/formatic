b3c9ab21f75f367558deda7d3be3f37022825121
Justin Deal <justin.deal@zapier.com>
2016-12-19 20:25:57 -0600

    start server on a free port

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
+var portscanner = require('portscanner');
 var config = require('./webpack.config');
 
-new WebpackDevServer(webpack(config), {
+var server = new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(3000, 'localhost', function (err) {
-  if (err) {
-    console.log(err);
-  }
+});
 
-  console.log('Listening at localhost:3000');
+portscanner.findAPortNotInUse(3000, 3100, '127.0.0.1', function(portErr, port) {
+  server.listen(port, '127.0.0.1', function (err) {
+    if (err) {
+      console.log(err);
+    }
+
+    console.log('Listening at localhost:' + port);
+  });
 });
