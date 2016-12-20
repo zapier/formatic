d8e52b2138e59f18049789302258860992f4ec9d
Justin Deal <justin.deal@zapier.com>
2016-12-19 20:41:40 -0600

    dynamic reload port

--- server.js
+++ server.js
@@ -3,13 +3,18 @@ var WebpackDevServer = require('webpack-dev-server');
 var portscanner = require('portscanner');
 var config = require('./webpack.config');
 
-var server = new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-});
-
 portscanner.findAPortNotInUse(3000, 3100, '127.0.0.1', function(portErr, port) {
+
+  config.entry[0] = config.entry[0].replace('3000', port);
+
+  console.log(config.entry);
+
+  var server = new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true
+  });
+
   server.listen(port, '127.0.0.1', function (err) {
     if (err) {
       console.log(err);
