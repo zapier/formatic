bbe7da028f0a4ed6974a39968f079b6e6adabe74
Justin Deal <justin.deal@zapier.com>
2018-10-11 17:35:31 -0500

    Bump to Next.js v7 because babel.config.js didn't work with Next.js v6

--- start-server.js
+++ start-server.js
@@ -1,5 +1,7 @@
+const babelConfig = require('./babel.config');
 require('@babel/register')({
   babelrc: false,
   presets: [['next/babel', { 'preset-env': { modules: 'commonjs' } }]],
+  plugins: babelConfig.plugins,
 });
 require('./server');
