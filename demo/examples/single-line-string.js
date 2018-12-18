906055bcc1a113a96db049b5125f0f18004f24c0
Caleb Peterson <caleb.peterson@zapier.com>
2018-12-18 14:38:16 -0500

    help text placement is now configurable

--- demo/examples/single-line-string.js
+++ demo/examples/single-line-string.js
@@ -1,6 +1,8 @@
 const fields = [
   {
     label: 'single line string',
+    helpText: 'help for the single line string field',
+    helpTextPlacement: 'after',
     type: 'single-line-string',
     key: 'single-line-string',
     placeholder: 'type something...',
@@ -8,6 +10,7 @@ const fields = [
   },
   {
     label: 'Name (R)',
+    helpText: 'help for the required name field',
     type: 'unicode',
     key: 'name',
     required: true,
