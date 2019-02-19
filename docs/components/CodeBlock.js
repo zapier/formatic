924179dd043cb144c541f789a9f0049f2bca7170
Justin Deal <justin.deal@zapier.com>
2019-02-19 10:13:03 -0600

    Shrink font for CodeBlock
    Font was looking a little big at 16px.

--- docs/components/CodeBlock.js
+++ docs/components/CodeBlock.js
@@ -1,11 +1,20 @@
-import React from 'react';
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
 import SyntaxHighlighter from 'react-syntax-highlighter/prism';
 import { atomDark } from 'react-syntax-highlighter/styles/prism';
 
+const styles = {
+  codeBlock: css({
+    fontSize: 14,
+  }),
+};
+
 const CodeBlock = props => (
-  <SyntaxHighlighter language={props.language || 'plain'} style={atomDark}>
-    {props.children}
-  </SyntaxHighlighter>
+  <div css={styles.codeBlock}>
+    <SyntaxHighlighter language={props.language || 'plain'} style={atomDark}>
+      {props.children}
+    </SyntaxHighlighter>
+  </div>
 );
 
 export default CodeBlock;
