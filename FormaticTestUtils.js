666b7ad8f76ede83c097b039d465aafb4fabf719
Justin Deal <justin.deal@zapier.com>
2019-02-21 17:46:10 -0600

    Document function a bit

--- FormaticTestUtils.js
+++ FormaticTestUtils.js
@@ -3,6 +3,8 @@ import { mount } from 'enzyme';
 
 import Formatic from './lib/formatic';
 
+// Render some Formatic fields to HTML so we can do a snapshot test of them.
+// Pass in a callback so you can do assertions on the components as well.
 export const renderFieldsToHtml = (fields, handleComponent = () => {}) => {
   const config = Formatic.createConfig();
   const value = config.createRootValue({
