737daa65215ae8631f527bc216fcd18e728bdfbd
Justin Deal <justin.deal@zapier.com>
2019-04-24 09:11:38 -0500

    Add ReactiveValueContainer/useReactiveValue primitives
    This allows us to create something similar to React's context but with an important difference: you can react to changes to specific properties of an object rather than only being able to react to changes to the object as a whole. This makes it efficient to pass in a context value and make changes to parts of the tree without having it rerender all parts of the tree. Later commits will hopefully make use of this in `FormContainer` and `useField`.

---
+++ src/future/ReactiveValue.js
@@ -0,0 +1,157 @@
+import React, { useContext, useEffect, useRef, useState } from 'react';
+
+function isObject(value) {
+  return value != null && typeof value === 'object';
+}
+
+function getValueAt(key, value) {
+  if (isObject(value)) {
+    return value[key];
+  }
+  return undefined;
+}
+
+// Wrap a value so that we can subscribe to changes to specific properties of
+// that value and ignore properties that don't change.
+const createReactiveValue = defaultValue => {
+  let currentValue = defaultValue;
+  const listeners = [];
+  const listenersByKey = {};
+
+  function getValue() {
+    return currentValue;
+  }
+
+  function getCurrentValueAt(key) {
+    if (isObject(currentValue)) {
+      return currentValue[key];
+    }
+
+    return undefined;
+  }
+
+  function notifyForKey(key, newValueAtKey) {
+    listenersByKey[key].forEach(handler => {
+      handler(newValueAtKey);
+    });
+  }
+
+  // Set value at a key and notify all subscribers to that key and the root
+  // value.
+  function setValueAt(key, newValue) {
+    if (getCurrentValueAt(key) !== newValue) {
+      if (isObject(currentValue)) {
+        currentValue = {
+          ...currentValue,
+          [key]: newValue,
+        };
+      }
+      // Notify of actual value, which could potentially be different from
+      // the desired value. For example, if the root value is not an object, or
+      // it's a proxy. At that point, we're pretty much into unsupported
+      // territory though, and the behavior is unknown.
+      const trueNewValue = getCurrentValueAt(key);
+      if (listenersByKey[key]) {
+        notifyForKey(key, trueNewValue);
+      }
+      // Notify any root value subscribers.
+      listeners.forEach(handler => {
+        handler(currentValue);
+      });
+    }
+  }
+
+  // Set the root value. Don't notify root listeners, since we don't want to
+  // cause a loop. But... if there are any listeners of properties, notify them
+  // if those properties have changed.
+  function setValue(newValue) {
+    if (newValue !== currentValue) {
+      const previousValue = currentValue;
+      currentValue = newValue;
+      for (const key in listenersByKey) {
+        const newValueAtKey = getValueAt(key, newValue);
+        if (getValueAt(key, previousValue) !== newValueAtKey) {
+          notifyForKey(key, newValueAtKey);
+        }
+      }
+    }
+  }
+
+  function subscribe(handler) {
+    listeners.push(handler);
+    return () => {
+      const i = listeners.indexOf(handler);
+      listeners.splice(i, 1);
+    };
+  }
+
+  function subscribeAt(key, handler) {
+    if (!(key in listenersByKey)) {
+      listenersByKey[key] = [];
+    }
+    const listenersAtKey = listenersByKey[key];
+    listenersAtKey.push(handler);
+    return () => {
+      const i = listenersAtKey.indexOf(handler);
+      listenersAtKey.splice(i, 1);
+      if (listenersAtKey.length === 0) {
+        delete listenersByKey[key];
+      }
+    };
+  }
+
+  return {
+    getValue,
+    getValueAt: getCurrentValueAt,
+    setValue,
+    setValueAt,
+    subscribe,
+    subscribeAt,
+  };
+};
+
+const ReactiveValueContext = React.createContext();
+
+export function ReactiveValueContainer({ value, onChange, children }) {
+  // We want to use a ref so that we can hold onto a single instance of our
+  // value wrapper.
+  const valueRef = useRef(null);
+  // Initialize our wrapper once.
+  if (valueRef.current === null) {
+    valueRef.current = createReactiveValue(value);
+  }
+  // Any time we get a new value, set the wrapper's value to that value. The
+  // wrapper will ignore equal values, so no worries of a loop here.
+  useEffect(() => {
+    valueRef.current.setValue(value);
+  });
+  // Any time our wrapper's value changes, kick off our onChange handler.
+  useEffect(
+    () => {
+      return valueRef.current.subscribe(onChange);
+    },
+    [onChange]
+  );
+  return (
+    <ReactiveValueContext.Provider value={valueRef.current}>
+      {children}
+    </ReactiveValueContext.Provider>
+  );
+}
+
+export function useReactiveValue(key) {
+  const context = useContext(ReactiveValueContext);
+  const [value, setValue] = useState(() => context.getValueAt(key));
+  useEffect(
+    () => {
+      // Subscribe to changes to the property and set our value in state when
+      // that property changes.
+      return context.subscribeAt(key, setValue);
+    },
+    [key]
+  );
+  function setValueInContext(newValue) {
+    context.setValueAt(key, newValue);
+  }
+  return { value, setValue: setValueInContext };
+}
---
+++ src/future/ReactiveValue.test.js
@@ -0,0 +1,210 @@
+/*global describe, test, expect*/
+
+import React, { useState } from 'react';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+
+import { ReactiveValueContainer, useReactiveValue } from './ReactiveValue';
+
+afterEach(cleanup);
+
+function createPureReactiveComponent(name, key, renderSpy) {
+  const PureReactiveComponent = React.memo(function PureReactiveComponent() {
+    const { value, setValue } = useReactiveValue(key);
+    if (renderSpy) {
+      renderSpy();
+    }
+    function coerceSetValue(newValue) {
+      if (typeof value === 'number') {
+        return setValue(parseInt(newValue));
+      }
+      return setValue(newValue);
+    }
+    return (
+      <input
+        data-testid={key}
+        onChange={event => coerceSetValue(event.target.value)}
+        type="text"
+        value={value}
+      />
+    );
+  });
+  PureReactiveComponent.displayName = name;
+  return PureReactiveComponent;
+}
+
+test('should notify of a change to a property', () => {
+  const onChangeSpy = jest.fn();
+  const FirstName = createPureReactiveComponent('FirstName', 'firstName');
+  const { getByTestId } = render(
+    <ReactiveValueContainer
+      onChange={onChangeSpy}
+      value={{ firstName: '', lastName: '' }}
+    >
+      <FirstName />
+    </ReactiveValueContainer>
+  );
+  fireEvent.change(getByTestId('firstName'), {
+    target: { value: 'Joe' },
+  });
+  expect(onChangeSpy.mock.calls[0]).toEqual([
+    {
+      firstName: 'Joe',
+      lastName: '',
+    },
+  ]);
+});
+
+test(`should avoid signaling hook for properties that don't change`, () => {
+  const onChangeSpy = jest.fn();
+  const firstNameRenderSpy = jest.fn();
+  const lastNameRenderSpy = jest.fn();
+  const FirstName = createPureReactiveComponent(
+    'FirstName',
+    'firstName',
+    firstNameRenderSpy
+  );
+  const LastName = createPureReactiveComponent(
+    'LastName',
+    'lastName',
+    lastNameRenderSpy
+  );
+  const { getByTestId } = render(
+    <ReactiveValueContainer
+      onChange={onChangeSpy}
+      value={{ firstName: '', lastName: '' }}
+    >
+      <FirstName />
+      <LastName />
+    </ReactiveValueContainer>
+  );
+  fireEvent.change(getByTestId('firstName'), {
+    target: { value: 'Joe' },
+  });
+  expect(onChangeSpy.mock.calls[0]).toEqual([
+    {
+      firstName: 'Joe',
+      lastName: '',
+    },
+  ]);
+  expect(firstNameRenderSpy.mock.calls.length).toBe(2);
+  expect(lastNameRenderSpy.mock.calls.length).toBe(1);
+});
+
+test(`should avoid unnecessary renders as controlled component`, () => {
+  const firstNameRenderSpy = jest.fn();
+  const lastNameRenderSpy = jest.fn();
+  const FirstName = createPureReactiveComponent(
+    'FirstName',
+    'firstName',
+    firstNameRenderSpy
+  );
+  const LastName = createPureReactiveComponent(
+    'LastName',
+    'lastName',
+    lastNameRenderSpy
+  );
+  function App() {
+    const [rootValue, setRootValue] = useState({
+      firstName: '',
+      lastName: '',
+    });
+    function onUpperCase() {
+      setRootValue({
+        firstName: rootValue.firstName.toUpperCase(),
+        lastName: rootValue.lastName.toUpperCase(),
+      });
+    }
+    return (
+      <div>
+        <ReactiveValueContainer onChange={setRootValue} value={rootValue}>
+          <FirstName />
+          <LastName />
+        </ReactiveValueContainer>
+        <input data-testid="upperCase" onClick={onUpperCase} type="button" />
+      </div>
+    );
+  }
+
+  const { getByTestId } = render(<App />);
+  fireEvent.change(getByTestId('firstName'), {
+    target: { value: 'Joe' },
+  });
+  expect(firstNameRenderSpy.mock.calls.length).toBe(2);
+  expect(lastNameRenderSpy.mock.calls.length).toBe(1);
+  fireEvent.click(getByTestId('upperCase'));
+  expect(firstNameRenderSpy.mock.calls.length).toBe(3);
+  expect(lastNameRenderSpy.mock.calls.length).toBe(1);
+});
+
+test(`should work with nesting, with no wasted renders`, () => {
+  const onChangeSpy = jest.fn();
+  const firstNameRenderSpy = jest.fn();
+  const lastNameRenderSpy = jest.fn();
+  const nameRenderSpy = jest.fn();
+  const ageRenderSpy = jest.fn();
+  const FirstName = createPureReactiveComponent(
+    'FirstName',
+    'firstName',
+    firstNameRenderSpy
+  );
+  const LastName = createPureReactiveComponent(
+    'LastName',
+    'lastName',
+    lastNameRenderSpy
+  );
+  const Age = createPureReactiveComponent('Age', 'age', ageRenderSpy);
+  function Name({ children }) {
+    const { value, setValue } = useReactiveValue('name');
+    nameRenderSpy();
+    return (
+      <ReactiveValueContainer onChange={setValue} value={value}>
+        {children}
+      </ReactiveValueContainer>
+    );
+  }
+  const { getByTestId } = render(
+    <ReactiveValueContainer
+      onChange={onChangeSpy}
+      value={{ name: { firstName: '', lastName: '' }, age: 50 }}
+    >
+      <Name>
+        <FirstName />
+        <LastName />
+      </Name>
+      <Age />
+    </ReactiveValueContainer>
+  );
+  fireEvent.change(getByTestId('firstName'), {
+    target: { value: 'Joe' },
+  });
+  expect(onChangeSpy.mock.calls[0]).toEqual([
+    {
+      name: {
+        firstName: 'Joe',
+        lastName: '',
+      },
+      age: 50,
+    },
+  ]);
+  expect(firstNameRenderSpy.mock.calls.length).toBe(2);
+  expect(lastNameRenderSpy.mock.calls.length).toBe(1);
+  expect(nameRenderSpy.mock.calls.length).toBe(2);
+  expect(ageRenderSpy.mock.calls.length).toBe(1);
+  fireEvent.change(getByTestId('age'), {
+    // See above where we coerce value.
+    target: { value: '51' },
+  });
+  expect(onChangeSpy.mock.calls[1]).toEqual([
+    {
+      name: {
+        firstName: 'Joe',
+        lastName: '',
+      },
+      age: 51,
+    },
+  ]);
+  expect(firstNameRenderSpy.mock.calls.length).toBe(2);
+  expect(lastNameRenderSpy.mock.calls.length).toBe(1);
+  expect(nameRenderSpy.mock.calls.length).toBe(2);
+  expect(ageRenderSpy.mock.calls.length).toBe(2);
+});
